a65479be7abd92e6f71bf30f143bcfe9dabae7cd
farcasnicu <nicolae.farcas@softvision.ro>
2016-12-22 19:07:00 +0200

    Initial commit

---
+++ views/footer.php
@@ -0,0 +1,13 @@
+<?php
+    echo '
+        <footer class="text-muted">
+        <div class="container">
+            <p class="float-xs-right">
+            <a href="#">Back to top</a>
+            </p>
+            <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+            <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+        </div>
+        </footer>
+    '
+?>
